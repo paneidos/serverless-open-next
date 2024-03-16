2f28ebf42a443a0cf6532f7158cc5b21d624740c
Sernin van de Krol <serninpc@paneidos.net>
2024-03-16 18:06:16 +0100

    Create plugin package structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export default class ServerlessOpenNext {
+    constructor(serverless, options, { log }) {
+        this.serverless = serverless
+        this.options = options
+        this.log = log
+    }
+}
