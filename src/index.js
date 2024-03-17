6e3d1894fce240fa9ce3d58e2a30105cc65551b4
Sernin van de Krol <serninpc@paneidos.net>
2024-03-17 19:13:29 +0100

    Create server function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,75 @@
+import { build } from 'open-next/build.js'
+import archiver from 'archiver';
+import fs from 'fs';
+
 export default class ServerlessOpenNext {
     constructor(serverless, options, { log }) {
         this.serverless = serverless
         this.options = options
         this.log = log
+
+        this.commands = {
+            'open-next': {
+                commands: {
+                    addFunctions: {lifecycleEvents: ['addFunctions']},
+                    buildFunctions: {lifecycleEvents: ['build', 'package']},
+                }
+            }
+        }
+
+        this.hooks = {
+            "before:package:initialize": () => this.serverless.pluginManager.spawn('open-next:addFunctions'),
+            "before:package:createDeploymentArtifacts": () => this.serverless.pluginManager.spawn('open-next:buildFunctions'),
+            "before:package:function:package": () => this.serverless.pluginManager.spawn('open-next:buildFunctions'),
+            "open-next:addFunctions:addFunctions": this.addFunctions.bind(this),
+            "open-next:buildFunctions:build": this.buildFunctions.bind(this),
+            "open-next:buildFunctions:package": this.packageFunctions.bind(this),
+        }
+    }
+
+    async buildFunctions() {
+        await build({
+            dangerous: {
+                disableIncrementalCache: true,
+                disableDynamoDBCache: true
+            }
+        })
+    }
+
+    packageFunction(name) {
+        return new Promise((resolve, reject) => {
+            const archive = archiver('zip', {})
+            const output = fs.createWriteStream(`.open-next/${name}.zip`)
+            output.on('close', function() {
+                resolve()
+            })
+            archive.on('error', reject)
+            archive.pipe(output)
+            archive.glob('**', {cwd: `.open-next/${name}`})
+            archive.glob('.next/**', {cwd: `.open-next/${name}`})
+            archive.glob('.open-next/**', {cwd: `.open-next/${name}`})
+            archive.finalize()
+        })
+    }
+
+    async packageFunctions() {
+        await Promise.all([
+            this.packageFunction('server-function'),
+        ])
+    }
+
+    addFunctions() {
+        const functions = {
+            server: {
+                handler: 'index.handler',
+                events: [],
+                url: true,
+                package: {
+                    individually: true,
+                    artifact: '.open-next/server-function.zip'
+                }
+            }
+        }
+        this.serverless.service.functions.server = functions.server
     }
 }
